cd818c66ac0f1b5746135105f3dccf80fcfbd650
ChiuanTest-01 <05156155@scu.edu.tw>
2019-01-12 21:01:53 +0800

    make it better

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from linebot.exceptions import (
 from linebot.models import *
 
 import requests 
-
+from BeautifulSoup import BeautifulSoup
 from urllib.request import urlretrieve
 
 import urllib3
