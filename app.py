a3dc447ed24d94c3e72d41a499250e53bdf2a669
ChiuanTest-01 <05156155@scu.edu.tw>
2019-01-19 20:29:04 +0800

    make it better

--- app.py
+++ app.py
@@ -56,6 +56,26 @@ def movie():
         content += '{}\n{}\n'.format(title, link)
     return content
 # 處理訊息
+
+def news():
+    # 下載 Yahoo 首頁內容
+    r = requests.get('https://tw.yahoo.com/')
+    # 確認是否下載成功
+    if r.status_code == requests.codes.ok:
+    # 以 BeautifulSoup 解析 HTML 程式碼
+      soup = BeautifulSoup(r.text, 'html.parser')
+
+    # 以 CSS 的 class 抓出各類頭條新聞
+      stories = soup.find_all('a', class_='story-title')
+      content=""
+
+      for s in stories[:5]:
+        title=s.text
+        href=s.get('href')
+        content += '{}\n{}\n'.format(title, href)
+    return content
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
