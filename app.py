39323ea28a04e568547f4b8f113816e90080c8c3
ChiuanTest-01 <tusna08124@gmail.com>
2019-06-08 19:45:25 +0800

    make it better

--- app.py
+++ app.py
@@ -322,216 +322,230 @@ def handle_message(event):
 
         line_bot_api.reply_message(event.reply_token, carousel_template_message)
 
-    elif event.message.text=="新新抽":
-        carousel=CarouselContainer(
-        contents=[
-            FlexComponent(
+
+    elif event.message.text == "選單":
+
+        bubble = BubbleContainer(
 
             direction='ltr',
-            hero=ImageComponent(
-                url='https://example.com/cafe.jpg',
-                size='full',
-                aspect_ratio='20:13',
-                aspect_mode='cover',
-                action=URIAction(uri='http://example.com', label='label')
-            ),
-            body=BoxComponent(
-                layout='vertical',
-                contents=[
-                    # title
-                    TextComponent(text='Brown Cafe', weight='bold', size='xl'),
-                    # review
-                    BoxComponent(
-                        layout='baseline',
-                        margin='md',
-                        contents=[
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                            IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                            TextComponent(text='4.0', size='sm', color='#999999', margin='md',
-                                          flex=0)
-                        ]
-                    ),
-                    # info
-                    BoxComponent(
-                        layout='vertical',
-                        margin='lg',
-                        spacing='sm',
-                        contents=[
-                            BoxComponent(
-                                layout='baseline',
-                                spacing='sm',
-                                contents=[
-                                    TextComponent(
-                                        text='Place',
-                                        color='#aaaaaa',
-                                        size='sm',
-                                        flex=1
-                                    ),
-                                    TextComponent(
-                                        text='Shinjuku, Tokyo',
-                                        wrap=True,
-                                        color='#666666',
-                                        size='sm',
-                                        flex=5
-                                    )
-                                ],
-                            ),
-                            BoxComponent(
-                                layout='baseline',
-                                spacing='sm',
-                                contents=[
-                                    TextComponent(
-                                        text='Time',
-                                        color='#aaaaaa',
-                                        size='sm',
-                                        flex=1
-                                    ),
-                                    TextComponent(
-                                        text="10:00 - 23:00",
-                                        wrap=True,
-                                        color='#666666',
-                                        size='sm',
-                                        flex=5,
-                                    ),
-                                ],
-                            ),
-                        ],
-                    )
-                ],
-            ),
-            footer=BoxComponent(
-                layout='vertical',
-                spacing='sm',
-                contents=[
-                    # callAction, separator, websiteAction
-                    SpacerComponent(size='sm'),
-                    # callAction
-                    ButtonComponent(
-                        style='link',
-                        height='sm',
-                        action=URIAction(label='CALL', uri='tel:000000'),
-                    ),
-                    # separator
-                    SeparatorComponent(),
-                    # websiteAction
-                    ButtonComponent(
-                        style='link',
-                        height='sm',
-                        action=URIAction(label='WEBSITE', uri="https://example.com")
-                    )
-                ]
-            ),
-        ),
-            FlexComponent(
 
-                direction='ltr',
-                hero=ImageComponent(
-                    url='https://example.com/cafe.jpg',
-                    size='full',
-                    aspect_ratio='20:13',
-                    aspect_mode='cover',
-                    action=URIAction(uri='http://example.com', label='label')
-                ),
-                body=BoxComponent(
-                    layout='vertical',
-                    contents=[
-                        # title
-                        TextComponent(text='Brown Cafe', weight='bold', size='xl'),
-                        # review
-                        BoxComponent(
-                            layout='baseline',
-                            margin='md',
-                            contents=[
-                                IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                                IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                                IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                                IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                                IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                                TextComponent(text='4.0', size='sm', color='#999999', margin='md',
-                                              flex=0)
-                            ]
-                        ),
-                        # info
-                        BoxComponent(
-                            layout='vertical',
-                            margin='lg',
-                            spacing='sm',
-                            contents=[
-                                BoxComponent(
-                                    layout='baseline',
-                                    spacing='sm',
-                                    contents=[
-                                        TextComponent(
-                                            text='Place',
-                                            color='#aaaaaa',
-                                            size='sm',
-                                            flex=1
-                                        ),
-                                        TextComponent(
-                                            text='Shinjuku, Tokyo',
-                                            wrap=True,
-                                            color='#666666',
-                                            size='sm',
-                                            flex=5
-                                        )
-                                    ],
-                                ),
-                                BoxComponent(
-                                    layout='baseline',
-                                    spacing='sm',
-                                    contents=[
-                                        TextComponent(
-                                            text='Time',
-                                            color='#aaaaaa',
-                                            size='sm',
-                                            flex=1
-                                        ),
-                                        TextComponent(
-                                            text="10:00 - 23:00",
-                                            wrap=True,
-                                            color='#666666',
-                                            size='sm',
-                                            flex=5,
-                                        ),
-                                    ],
-                                ),
-                            ],
-                        )
-                    ],
-                ),
-                footer=BoxComponent(
-                    layout='vertical',
-                    spacing='sm',
-                    contents=[
-                        # callAction, separator, websiteAction
-                        SpacerComponent(size='sm'),
-                        # callAction
-                        ButtonComponent(
-                            style='link',
-                            height='sm',
-                            action=URIAction(label='CALL', uri='tel:000000'),
-                        ),
-                        # separator
-                        SeparatorComponent(),
-                        # websiteAction
-                        ButtonComponent(
-                            style='link',
-                            height='sm',
-                            action=URIAction(label='WEBSITE', uri="https://example.com")
-                        )
-                    ]
-                ),
-            )
-            ]
+            body=BoxComponent(
+
+                layout='vertical',
+
+                contents=[
+
+                    # title
+
+                    TextComponent(text='功能推薦', weight='bold', size='xl'),
+
+                    # BoxComponent(
+
+                    #     layout='vertical',
+
+                    #     margin='lg',
+
+                    #     spacing='sm',
+
+                    #     contents=[
+
+                    #         BoxComponent(
+
+                    #             layout='baseline',
+
+                    #             spacing='sm',
+
+                    #             contents=[
+
+                    #                 TextComponent(
+
+                    #                     text='類別',
+
+                    #                     color='#aaaaaa',
+
+                    #                     size='sm',
+
+                    #                     flex=1
+
+                    #                 ),
+
+                    #                 TextComponent(
+
+                    #                     text='休閒娛樂',
+
+                    #                     wrap=True,
+
+                    #                     color='#666666',
+
+                    #                     size='sm',
+
+                    #                     flex=5
+
+                    #                 )
+
+                    #             ],
+
+                    #         ),
+
+                    #     ],
+
+                    # )
+
+                ],
+
+            ),
+
+            footer=BoxComponent(
+
+                layout='vertical',
+
+                spacing='xs',
+
+                contents=[
+
+                    # first row
+
+                    BoxComponent(
+
+                        layout='horizontal',
+
+                        spacing='xs',
+
+                        contents=[
+
+                            # callAction, separator, websiteAction
+
+                            SpacerComponent(size='sm'),
+
+                            # callAction
+
+                            ImageComponent(
+
+                                url='https://pic.pimg.tw/ma16zaq2000/1480521926-2602114417_l.jpg',
+
+                                size='full',
+
+                                aspect_ratio='20:13',
+
+                                aspect_mode='cover',
+
+                                action=MessageAction(label='餐廳', text='餐廳')
+
+                            ),
+
+                            # separator
+
+                            SeparatorComponent(),
+
+                            # websiteAction
+
+                            ImageComponent(
+
+                                url='https://assets.sandsresortsmacao.cn/content/dam/parisianmacao/hotel/rooms-suites/deluxe-room/pm_deluxe-room_800x480_1.jpg',
+
+                                size='full',
+
+                                aspect_ratio='20:13',
+
+                                aspect_mode='cover',
+
+                                action=MessageAction(label='住宿',
+
+                                                     text='住宿')
+
+                            )
+
+                        ]
+
+                    ),
+
+                    # second row
+
+                    BoxComponent(
+
+                        layout='horizontal',
+
+                        spacing='xs',
+
+                        contents=[
+
+                            # callAction, separator, websiteAction
+
+                            SpacerComponent(size='sm'),
+
+                            # callAction
+
+                            ImageComponent(
+
+                                url='https://ws.www.gov.tw/001/Upload/305/relpic/7982/267588/aa983071-1bb3-4054-b817-27e2804f17a6.jpg',
+
+                                size='full',
+
+                                aspect_ratio='20:13',
+
+                                aspect_mode='cover',
+
+                                action=MessageAction(label='景點', text='景點')
+
+                            ),
+
+                            # separator
+
+                            SeparatorComponent(),
+
+                            # websiteAction
+
+                            ImageComponent(
+
+                                url='https://pbs.twimg.com/media/C01BweoVQAAbzma.jpg',
+
+                                size='full',
+
+                                aspect_ratio='20:13',
+
+                                aspect_mode='cover',
+
+                                action=MessageAction(label='商店',
+
+                                                     text='商店')
+
+                            )
+
+                        ]
+
+                    ),
+
+                ]
+
+            ),
+
         )
-        message = FlexSendMessage(alt_text="hello", contents=carousel)
-        line_bot_api.reply_message(
-            event.reply_token,
-            message
+
+        message = FlexSendMessage(alt_text="hello", contents=bubble)
+
+        bubble1 = BubbleContainer(
+
+            direction='ltr',
+
+            hero=ImageComponent(
+
+                url='https://banner2.kisspng.com/20180607/luv/kisspng-mapquest-computer-icons-logo-google-maps-5b18bd44de3033.6727374215283479729101.jpg',
+
+                size='full',
+
+                aspect_ratio='20:13',
+
+                aspect_mode='cover',
+
+                action=URIAction(uri='line://nv/location', label='label')
+
+            ),
+
         )
+
+        message1 = FlexSendMessage(alt_text="hello", contents=bubble1)
+
+        line_bot_api.reply_message(event.reply_token, [message, message1])
 
     elif event.message.text == "新聞":
         a=news()
@@ -551,13 +565,16 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text="Flex message", contents=bubble))
 
     elif event.message.text == "測試用2":
-        Carousel = CarouselContainer(body=BoxComponent(layout='vertical',
+        Carousel = CarouselContainer(BubbleContainer(body=BoxComponent(layout='vertical',
           contents=[
             TextComponent(text='更新位置?'),
             ButtonComponent(action=URIAction(label='請給予位置', uri='line://nv/location'))
-        , TextComponent(text='更新位置222?'),
-            ButtonComponent(action=URIAction(label='請給予位置222', uri='line://nv/location'))
-          ]))
+
+          ] )),
+
+
+
+                                     )
 
         line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text="Flex message", contents=Carousel))
 
