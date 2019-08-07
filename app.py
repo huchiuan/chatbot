1d1765a74bdb863c017d7282b66ea5c6bb086355
ChiuanTest-01 <tusna08124@gmail.com>
2019-08-07 17:08:15 +0800

    <token test>

--- app.py
+++ app.py
@@ -24,8 +24,10 @@ app = Flask(__name__)
 
 # Channel Access Token
 
-handler=os.environ.get
-line_bot_api=os.environ.get
+# Channel Access Token
+line_bot_api = LineBotApi(os.environ.get('ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.environ.get('CHANNEL_SECERT'))
 #test
 
 
